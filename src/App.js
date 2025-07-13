138f83fb9140c32f3fde2c80c641b01cc21ba8f6
Meet <meetnp007@gmail.com>
2025-07-13 16:30:49 -0400

    Password

--- src/App.js
+++ src/App.js
@@ -307,8 +307,8 @@ const AppRoutes = () => (
     <Route path="/edit-product/:id" element={<PrivateRoute><EditProductPage /></PrivateRoute>} />
     <Route path="/vendor-orders" element={<PrivateRoute><VendorOrders /></PrivateRoute>} />
     <Route path="/verify-email" element={<VerifyEmail />} />
-    <Route path="/vendor/forgot-password" element={<VendorForgotPasswordPage />} />
-    <Route path="/vendor/reset-password" element={<VendorResetPasswordPage />} />
+    <Route path="/forgot-password" element={<VendorForgotPasswordPage />} />
+    <Route path="/reset-password" element={<VendorResetPasswordPage />} />
   </Routes>
 );
 
